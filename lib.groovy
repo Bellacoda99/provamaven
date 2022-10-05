3cee60099163e7833e7a26930bb486b009ffa8f0
Bellacoda99 <104764811+Bellacoda99@users.noreply.github.com>
2022-10-05 16:34:19 +0200

    Update lib.groovy

--- lib.groovy
+++ lib.groovy
@@ -1,1 +1,19 @@
-
+pipeline {
+  agent any
+  tools {
+    maven 'mvnapp'
+  }
+  
+  def lib(){
+    sh 'echo ciao > maven.txt'
+  }
+    
+    
+  stages {
+    stage('Java') {
+      steps {
+        sh 'mvn package'
+      }
+    }
+  }
+}
